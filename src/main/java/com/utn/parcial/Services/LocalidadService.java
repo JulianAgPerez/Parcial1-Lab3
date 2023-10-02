1885d6dee1218c7a96e9b4dd53bd964f44fdf358
JulianAgPerez <129700990+JulianAgPerez@users.noreply.github.com>
2023-10-02 20:24:46 -0300

    Update LocalidadService.java

--- src/main/java/com/utn/parcial/Services/LocalidadService.java
+++ src/main/java/com/utn/parcial/Services/LocalidadService.java
@@ -1,9 +1,7 @@
 package com.utn.parcial.Services;
 
 import com.utn.parcial.Entities.Localidad;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface LocalidadService extends BaseService<Localidad,Long> {
 
 }
