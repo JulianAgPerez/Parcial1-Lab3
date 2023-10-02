42166c6ac6e92b643e0f4f0067339bcc29c6318b
JulianAgPerez <129700990+JulianAgPerez@users.noreply.github.com>
2023-10-02 20:24:04 -0300

    Add files via upload

---
+++ src/main/java/com/utn/parcial/Controllers/LocalidadController.java
@@ -0,0 +1,14 @@
+package com.utn.parcial.Controllers;
+
+
+import com.utn.parcial.Entities.Localidad;
+import com.utn.parcial.Services.LocalidadServiceImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "api/v1/localidades")
+public class LocalidadController extends BaseControllerImpl<Localidad, LocalidadServiceImpl>{
+}
